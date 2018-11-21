e652f85f88f862adccdd9d0b896f8140af4f485e
suxx <suxx9999@qq.com>
2018-11-21 12:30:40 +0800

    作者组件源代码

---
+++ Example/Pods/Target Support Files/FFAuthorKit/FFAuthorKit-umbrella.h
@@ -0,0 +1,22 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FFAuthorDetailController.h"
+#import "AuthorAPIRequest.h"
+#import "FFAuthorCell.h"
+#import "FFAuthorController.h"
+#import "FFAuthorListReformer.h"
+#import "FFAuthorKit.h"
+
+FOUNDATION_EXPORT double FFAuthorKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char FFAuthorKitVersionString[];
+
---
+++ FFAuthorKit/Classes/FFAuthorKit.h
@@ -0,0 +1,21 @@
+//
+//  FFAuthorKit.h
+//  Pods
+//
+//  Created by suxx on 2018/11/20.
+//
+
+#ifndef FFAuthorKit_h
+#define FFAuthorKit_h
+
+#import <Masonry/Masonry.h>
+#import <FFToolsKit/FFToolsKit-umbrella.h>
+#import <FFConfigsKit/FFConfigsKit-umbrella.h>
+#import <FFCategoryKit/FFCategoryKit-umbrella.h>
+#import <FFMainViewKit/FFMainViewKit-umbrella.h>
+#import <FFReformerKeysKit/FFReformerKeysKit-umbrella.h>
+#import <ReactiveCocoa/ReactiveCocoa-umbrella.h>
+#import <YYWebImage/YYWebImage.h>
+#import <FFAPIsKit/FFAPIsKit-umbrella.h>
+
+#endif /* FFAuthorKit_h */
