065f6253473e723273c9a9c46ec45159bffaa7ae
suxx <suxx9999@qq.com>
2018-11-21 11:56:30 +0800

    Initial commit

---
+++ Example/FFAuthorKit/FFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFViewController.h
+//  FFAuthorKit
+//
+//  Created by suxx9999@qq.com on 11/21/2018.
+//  Copyright (c) 2018 suxx9999@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFViewController : UIViewController
+
+@end
---
+++ Example/FFAuthorKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FFAuthorKit
+//
+//  Created by suxx9999@qq.com on 11/21/2018.
+//  Copyright (c) 2018 suxx9999@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "FFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FFAppDelegate class]));
+    }
+}
